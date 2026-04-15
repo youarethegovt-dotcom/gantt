f061e94068e1085d7232f76b19743688dd64c2f5
youarethegovt-dotcom <youarethegovt@gmail.com>
2026-04-15 15:19:30 -0500

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const SUPABASE_URL = 'https://cfyijoveheyznubkyool.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNmeWlqb3ZlaGV5em51Ymt5b29sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc1NzIzNzcsImV4cCI6MjA1MzE0ODM3N30.kSEmBJOspFST_gVQasBGSXxMskFXBIcmsP9jBJsfLw0';
+const SUPABASE_ANON_KEY = 'sb_publishable_JW72QkTRQIi_oiMV_w2rKw_R6nA-F8D';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
