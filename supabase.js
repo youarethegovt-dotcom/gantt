d5aaf5cacb702ac3e62e4253f31111eeda2e8e0b
youarethegovt-dotcom <youarethegovt@gmail.com>
2026-04-15 14:48:00 -0500

    Delete supabase.js

--- supabase.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { AuthWrapper } from './App.jsx';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <AuthWrapper />
-  </React.StrictMode>
-);
